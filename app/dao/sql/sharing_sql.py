f627a3efe709d31a1f748809caf2a4a55c16e9f3
JyQAQ <961999367@qq.com>
2019-02-03 22:45:13 +0800

    吉远 完成测评发布的sql写入以及转换标签id和转换产品id辅助接口的实现

--- app/dao/sql/sharing_sql.py
+++ app/dao/sql/sharing_sql.py
@@ -3,5 +3,8 @@ sharing_sql = {
     "evaluationIndex":"",
     "graphicDy":"",
     "graphicDairy":"",
-    "graphicTest":""
+    "graphicTest1":"insert into commodity_test_main (commodity_id,title,content,user_id,tags,img) VALUES({com_id},'{main_title}','{main_content}',{user_id},'{tags_id}','{img}')",
+    "graphicTest2":"INSERT into commodity_test_subtitle (title,content,main_id) VALUES ('{title1}','{content1}',{test_id}),('{title2}','{content2}',{test_id})",
+    "getTag":"SELECT tags_id from tags WHERE tags_content='{tag_name}'",
+    "getCom":"SELECT id from commodity WHERE commodity_name='{com_name}'"
 }
--- app/service/sharing_interface.py
+++ app/service/sharing_interface.py
@@ -25,7 +25,14 @@ def graphic_Dairy(con):
 
 # 发布测评的封装方法
 def graphic_Test(con):
-    if con.get('title') and con.get('content') and con.get('img') and con.get('tag') and con.get('title1') and con.get('content1') and con.get('title2') and con.get('content2'):
+    if con.get('com') and con.get('title') and con.get('content') and con.get('img') and con.get('tag') and con.get('title1') and con.get('content1') and con.get('title2') and con.get('content2'):
+        # 转化标签名
+        tag_id = get_Tag(con['tag'])
+        con['tag_id'] = tag_id
+        # 转化产品名
+        com_id = get_Com(con['com'])
+        con['com_id'] = com_id
+        print(con)
         res = graphicTest(con)
         if res:
             return {"status_code":"10008","status_text":"发布成功"}
@@ -41,3 +48,29 @@ def evaluation_Index():
         return res
     else:
         return {"status_code":"40004","status_text":"系统错误"}
+
+# 获取标签id
+def get_Tag(tags):
+    tag_id = []
+    for tag_name in tags:
+        tagid = getTag(tag_name)
+        if tagid:
+            tag_id.append(tagid['tags_id'])
+        else:
+            return {"status_code": "40004", "status_text": "系统错误"}
+    return tag_id
+
+#获取产品id
+def get_Com(com):
+    com_id = getCom(com)
+    if com_id:
+        return com_id['id']
+    else:
+        return {"status_code": "40004", "status_text": "系统错误"}
+
+if __name__ == '__main__':
+    u = {'com':'大宝SOD','user_id':21,'title':'test','content':'this is test','title1':'title-1','content1':'content-1','title2':'title-2','content2':'content-2','img':'img/myimg.jpg','type':'test','tag':['保湿','提亮肤色']}
+    # u = ['保湿','提亮肤色']
+    # u = '大宝SOD'
+    res=graphic_Test(u)
+    print(res)
